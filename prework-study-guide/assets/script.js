e2c2675ce60316192fc1b514b7320fd61719371f
JohnBrunson <JohnFBrunson@gmail.com>
2024-08-29 19:55:08 -0700

    Add JS feature -- Part 1

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,33 @@
+const topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+
+for (let i = 0; i < topics.length; i++) {
+    console.log(topics[i])
+};
+
+
+/* const topic = "Hello";
+
+if (topic === 'HTML') {
+    console.log("Let's Study HTML!");
+}
+else if (topic === 'CSS') {
+    console.log("Let's Study CSS!");
+}
+else if (topic === 'Git') {
+    console.log("Let's Study Git!");
+}
+else if (topic === 'Javascript') {
+    console.log("Let's Study Javascipt!");
+}
+else {
+    console.log('Please try again');
+}
+
+const shapes = ["triangle", "square", "pentagon", "circle"];
+//console.log (shapes[0]);
+
+for(let i = 0; i < shapes.length; i++) {
+    console.log (shapes[i])
+}
+
+*/
